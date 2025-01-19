a61020ed39a2d14875edd24e91154b1c7dbf1f97
Abdulazeez Abdulsamad <abdulazeezabdulsamad16@gmail.com>
2025-01-19 07:32:52 +0100

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { StudentSchema } from './schema/student.schema';
-// import { StudentService } from './student/student.service';
-// import { StudentController } from './student/student.controller';
-
-// @Module({
-//   imports: [
-//     MongooseModule.forRoot('mongodb+srv://mine:supor@cluster0.8lsutr0.mongodb.net/nest', {
-//       dbName: 'studentdb',
-//     }),
-//     MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
-//   ],
-//   controllers: [AppController, StudentController],
-//   providers: [AppService, StudentService],
-// })
-// export class AppModule {}
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -25,21 +6,23 @@ import { AppService } from './app.service';
 import { StudentSchema } from './schema/student.schema';
 import { StudentService } from './student/student.service';
 import { StudentController } from './student/student.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // Makes the configuration accessible globally
+      isGlobal: true, 
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('DATABASE_URL'), // Load the URL from .env
-        dbName: configService.get<string>('DATABASE_NAME'), // Load the DB name from .env
+        uri: configService.get<string>('DATABASE_URL'),
+        dbName: configService.get<string>('DATABASE_NAME'), 
       }),
       inject: [ConfigService],
     }),
     MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
+    AuthModule,
   ],
   controllers: [AppController, StudentController],
   providers: [AppService, StudentService],
